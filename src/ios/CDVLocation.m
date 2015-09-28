174c1f47b36ee31d568a7ff6f489e8c0a57b0dc4
Christopher Blum <christopher.blum@spellboy.com>
2015-09-29 00:52:46 +0200

    Enable background location updates in ios9

--- src/ios/CDVLocation.m
+++ src/ios/CDVLocation.m
@@ -137,7 +137,11 @@
         return;
     }
 #endif
-    
+
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9) {
+        self.locationManager.allowsBackgroundLocationUpdates = YES;
+    }
+
     // Tell the location manager to start notifying us of location updates. We
     // first stop, and then start the updating to ensure we get at least one
     // update, even if our location did not change.
